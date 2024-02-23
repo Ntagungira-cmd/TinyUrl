34f60d62c1009272dbe9ffc3518aab35a1ecc032
Ntagungira-cmd <ntagungiraali@gmail.com>
2024-02-23 19:28:02 +0200

    change base url

--- TinyUrlClient/src/utils/api.js
+++ TinyUrlClient/src/utils/api.js
@@ -1,6 +1,5 @@
-//base url
-// export const API_URL = "https://tinyurl-a9jg.onrender.com";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://tinyurl-a9jg.onrender.com";
+//export const API_URL = "http://localhost:8080";
 
 //retrieve token from local storage
 export const config = {
